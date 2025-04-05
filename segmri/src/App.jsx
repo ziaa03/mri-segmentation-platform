1087cfba19bb2ac9941d1409fd8275f3323ec344
ziaa03 <ziacitan04@gmail.com>
2025-04-05 12:24:41 +0800

    Got rid of App.css and made index.css the main css file

--- segmri/src/App.jsx
+++ segmri/src/App.jsx
@@ -1,1 +1,21 @@
-// handle routing here
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import LandingPage from './pages/LandingPage';
+import './index.css'; 
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      
+      <main className="flex-grow">
+        <LandingPage />
+      </main>
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
