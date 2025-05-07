acfa8f00a936a41b38536d45374d2358017dbf2a
ziaa03 <ziacitan04@gmail.com>
2025-05-07 15:22:40 +0800

    Refined header, file and user management pages

--- segmri/src/components/Header.jsx
+++ segmri/src/components/Header.jsx
@@ -8,26 +8,34 @@ const Header = () => {
           <div className="flex items-center">
             {/* Logo */}
             <Link to="/">
-            <h1 className="text-2xl font-semibold text-[#343231] mr-4">VisHeart</h1>
+              <h1 className={`text-2xl font-semibold mr-4 transition-colors`}>
+                <span className="text-[#74342B]">Vis</span>Heart
+              </h1>           
             </Link>
           </div>
           
           <nav className="hidden md:block">
-            <ul className="flex space-x-8">
+            <ul className="flex space-x-8 text-sm font-medium ">
               <li>
-                <a href="/" className="text-[#343231] hover:text-[#74342B] hover:underline transition-colors">HOME</a>
+                <a href="/" className="text-[#343231] hover:text-[#74342B]  transition-colors">HOME</a>
               </li>
               <li>
-                <a href="/uploads" className="text-[#343231] hover:text-[#74342B] hover:underline transition-colors">UPLOADS</a>
+                <a href="/uploads" className="text-[#343231] hover:text-[#74342B] transition-colors">UPLOADS</a>
               </li>
               <li>
-                <a href="/about-us" className="text-[#343231] hover:text-[#74342B] hover:underline transition-colors">ABOUT US</a>
+                <a href="/about-us" className="text-[#343231] hover:text-[#74342B] transition-colors">ABOUT US</a>
               </li>
               <li>
-                <a href="/login" className="text-[#343231] hover:text-[#74342B] hover:underline transition-colors">LOGIN</a>
+                <a href="/login" className="text-[#343231] hover:text-[#74342B] transition-colors">LOGIN</a>
               </li>
               <li>
-                <a href="/user-settings" className="text-[#343231] hover:text-[#74342B] hover:underline transition-colors">SETTINGS</a>
+                <a href="/user-settings" className="text-[#343231] hover:text-[#74342B] transition-colors">SETTINGS</a>
+              </li>
+              <li>
+                <a href="/user-management" className="text-[#343231] hover:text-[#74342B] transition-colors">USERS</a>    
+              </li>
+              <li>
+                <a href="/cardiac-analysis" className="text-[#343231] hover:text-[#74342B] transition-colors">CARDIAC ANALYSIS</a>
               </li>
             </ul>
           </nav>
