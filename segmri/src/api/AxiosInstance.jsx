a672efcdd34b3d88a2ac46dd8ca74aca10ffe35d
ziaa03 <ziacitan04@gmail.com>
2025-05-22 22:49:42 +0800

    Axios fix

--- segmri/src/api/AxiosInstance.jsx
+++ segmri/src/api/AxiosInstance.jsx
@@ -1,13 +1,13 @@
 // AxiosInstance.jsx
 import axios from 'axios';
 
-const URL = "localhost:3000"; 
+// const URL = "localhost:8000"; 
 
 const api = axios.create({
-  baseURL: `http://${URL}`,
+  // baseURL: http://${URL},
+  baseURL: 'https://cos30045.xyz',
   withCredentials: true, 
   timeout: 30000,
 });
 
 export default api;
-
