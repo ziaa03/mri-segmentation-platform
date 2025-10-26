efc45d2ba3b8f3f4d2cf7fcdb3ee325bc0b2562c
ziaa03 <ziacitan04@gmail.com>
2025-10-26 23:06:35 +0800

    Resized header

--- segmri/src/components/Header.jsx
+++ segmri/src/components/Header.jsx
@@ -110,7 +110,7 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-white backdrop-blur-sm border-b border-[#74342B]/10 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-14">
           
           {/* Logo */}
           <div className="flex items-center">
